686ab9bb87a7aca2f0d663fb359327be53678aaa
Md Imran Talukder <58259156+imran15995@users.noreply.github.com>
2022-04-11 19:49:55 +0600

    Add files via upload

---
+++ masala_on/footer.php
@@ -0,0 +1,24 @@
+<!--footer-->
+<footer class="footer" id="footer">
+ <div class="container"><br>
+  <div class="row">
+    <div class="col-sm-12 text-center">
+      <div class="social-icon">
+        <a href="index.php"><i class="fa fa-facebook"></i></a>
+        <a href="index.php"><i class="fa fa-twitter"></i></a>
+        <a href="index.php"><i class="fa fa-pinterest"></i></a>
+        <a href="index.php"><i class="fa fa-rss"></i></a>
+      </div>
+      <div class="copyright">
+        <p class="white"> copyright &copy; <b>Masala On</b></p>
+        <p><i class="fa fa-phone"></i> +880 1000 000 456 &nbsp;<i class="fa fa-envelope-o"></i> masalaon@diu.edu.bd</p>
+      </div>
+    </div>
+  </div>
+ </div>
+</footer>
+<!--end of footer-->
+</div>
+</body>
+<!---end of body-->
+</html>
---
+++ masala_on/index.php
@@ -0,0 +1,202 @@
+<?php
+require "header.php";
+?>
+
+<header class="header">
+    <div class="row">
+        <div class="col-md-12 text-center">
+   <a class="logo"><img src="img/logo2.png" alt="logo"></a>
+
+   </div>
+        <div  class="col-md-12 text-center">
+            <button id="rb1" type="button" onclick="window.location.href='reservation.php'" class="btn btn-outline-light btn-lg"><em>Make a Reservation Now!</em></button>
+        </div>
+    </div>
+</header>
+
+
+
+<!--about us section-->
+
+<section id="aboutus">
+
+ <div class="container">
+   <h3 class="text-center"><br><br>Masala On</h3>
+   <div class="row">
+<!--carousel-->
+     <div class="col-sm"><br><br>
+      	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+         <ol class="carousel-indicators">
+           <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+         </ol>
+        <div class="carousel-inner">
+           <div class="carousel-item active">
+             <img class="d-block w-100" src="img/3.jpeg" alt="First slide">
+           </div>
+           <div class="carousel-item">
+           <img class="d-block w-100" src="img/2.png" alt="Second slide">
+           </div>
+           <div class="carousel-item">
+           <img class="d-block w-100" src="img/5.jpeg" alt="Third slide">
+           </div>
+        </div>
+         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+           <span class="sr-only">Previous</span>
+         </a>
+         <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+           <span class="carousel-control-next-icon" aria-hidden="true"></span>
+           <span class="sr-only">Next</span>
+         </a>
+       </div><br><br>
+     </div>
+
+<!--end of carousel-->
+
+     <div class="col-sm">
+    	<div class="arranging"><br><hr>
+	<h4 class="text-center">Our Story</h4>
+	<p><br>The Masala On, first opened in 2018 at Shyamoli in Dhaka. In 2019 , the restaurant was awarded its first Michelin star and has retained it since.<br><br>
+	The same year, the restaurant relocated to the 6th floor of the Basundhara city and was recently redesigned by award-winning architectural firm Divercity.In warmer months of the year the restaurant moves to the 7th floor of the building, offering a unique outdoor setting with panoramic view of the Acropolis, Lycebettus hill and the city skyline.<br><br><br></p><hr>
+	</div>
+     </div>
+    </div><br>
+  </div>
+</section>
+<!--end of about us section-->
+
+<div class="header2">
+</div>
+
+<!----gallery -->
+<div class id="gallery"><br>  
+    <div class="container">
+    <h3 class="text-center"><br>Gallery<br><br></h3>
+        <div class="d-flex flex-row flex-wrap justify-content-center">
+           <div class="d-flex flex-column">
+              <img src="img/1.jpg" class="img-fluid" >
+              <img src="img/2.png" class="img-fluid">
+           </div>
+           <div class="d-flex flex-column">
+              <img src="img/3.jpeg" class="img-fluid">
+              <img src="img/4.jpeg" class="img-fluid">
+           </div>
+           <div class="d-flex flex-column">
+               <img src="img/5.jpeg" class="img-fluid">
+               <img src="img/6.jpeg" class="img-fluid">
+           </div>
+           <div class="d-flex flex-column">
+               <img src="img/7.jpeg" class="img-fluid">
+               <img src="img/8.jpeg" class="img-fluid">
+           </div>
+        </div>
+    </div>
+</div><br><br>
+<!----end of gallery -->
+
+<div class="container" id="reservation">
+    <h3 class="text-center"><br><br>Reservation<br><br></h3>
+    <img  src="img/16.jpg" class="img-fluid rounded">
+    <button id="reserve-btn" type="button" onclick="window.location.href='reservation.php'" class="btn btn-outline-dark btn-block btn-lg">Make a reservation Now!</button>
+        
+</div><br><br>
+
+<div class="header2">
+</div>
+
+<!-- main page map section-->
+<section class="map" id="footer">
+    <div class="container">
+    <h3 class="text-center"><br><br>Find us!</h3><br>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4342.848718056367!2d90.38772819695848!3d23.751447223641865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8bcd681372b%3A0x5c2b8755e4327004!2sBashundhara%20City%20Shopping%20Complex!5e0!3m2!1sen!2sbd!4v1608760308308!5m2!1sen!2sbd" style= "width:100%;  height:250px; border:0;" allowfullscreen></iframe>
+    
+        <div class="row staff">
+            <div class="col">
+            <h4><strong>Opening Hours</strong></h4>
+                       
+                <div class="signup-form">
+                    <form action="#footer" method="post">
+                        <div class="form-group">
+                            <label>Enter Date</label>
+                            <input type="date" class="form-control" name="date" placeholder="Date" required="required">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" name="check_schedule" class="btn btn-dark btn-block">Check Open Time</button>
+                        </div>
+                    </form>
+                    
+<?php
+
+if(isset($_POST['check_schedule'])){
+      
+require 'includes/dbh.inc.php';
+            
+$date= $_POST['date'];
+ 
+    $sql = "SELECT * FROM schedule WHERE date = '$date'";
+    $result = $conn->query($sql);
+    if ($result->num_rows == 1) {
+        while($row = $result->fetch_assoc()) {
+            echo"
+                <table class='table table-sm table-striped table-dark text-center'>
+                   <thead>
+                    <tr>
+                    <th scope='col'>Date</th>
+                    <th scope='col'>Open Time</th>
+                    <th scope='col'>Close Time</th>
+                    </tr>
+                   </thead>
+                   <tbody>
+                    <tr>
+                    <th scope='row'><em>". $date . "</em></th>
+                    <td>".$row['open_time']."</td>
+                    <td>".$row['close_time']."</td>
+                    </tr>
+                   </tbody>
+                </table>";
+                }
+            }
+        else{
+         echo"
+                <table class='table table-striped table-dark text-center'>
+                   <thead>
+                    <tr>
+                    <th scope='col'>Date</th>
+                    <th scope='col'>Open Time</th>
+                    <th scope='col'>Close Time</th>
+                    </tr>
+                   </thead>
+                   <tbody>
+                    <tr>
+                    <th scope='row'><em>". $date . "</em></th>
+                    <td>12:00</td>
+                    <td>00:00</td>
+                    </tr>
+                   </tbody>
+                </table>";
+            }
+         
+   //close connection
+   mysqli_close($conn);
+}
+?>
+                        
+                </div><br>
+            </div>
+
+            <div class="col">
+            <h4 class="text-right"><strong>Visit Us</strong></h4>
+            <p class="text-right"><strong>Masala On</strong><br><i class="fa fa-map-marker"></i>&nbsp; Basundhara Shopping complex, <br>Kawranbazar<br><br>Email:masalaon@diu.edu.bd<br>Phone: +880 1000 000 456</p>
+            </div>
+
+	</div>
+    </div>
+</section>
+<!--end of main page map section-->
+
+
+<?php
+require "footer.php";
+?>
